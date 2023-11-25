a324a21414eee9ec874dc3cf7fd7295dcead66f2
Pranjal Yadav <godricglow@gmail.com>
2023-11-25 19:23:19 +0530

    out to dvclive

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -90,7 +90,7 @@ def main():
     # Load the data.
     input_file = sys.argv[2]
     data_path = home_dir.as_posix() + input_file
-    output_path = home_dir.as_posix() + '/reports/figures'
+    output_path = home_dir.as_posix() + '/dvclive'
     pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
     
     TARGET = 'Class'
