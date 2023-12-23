482d129942dec6b2cf606389e9d37373135a31ad
Pranjal Yadav <godricglow@gmail.com>
2023-12-23 18:15:34 +0530

    changing to 8080

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def predict(input_data: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
